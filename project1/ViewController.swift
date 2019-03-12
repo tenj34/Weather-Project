8c02135019bf6f90f798a4f341e0d6d9cbf1795c
Juan Tenezaca <tenj34@gmail.com>
2019-03-11 20:28:52 -0400

    Initial Commit

---
+++ project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by Juan Tenezaca on 3/11/19.
+//  Copyright © 2019 Juan Tenezaca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
