7c7a56ff26a39b3b01aa7e69811438ea5676ba35
Juan Tenezaca <tenj34@gmail.com>
2019-03-20 08:07:39 -0400

    Quick small weather project

--- project1/ViewController.swift
+++ project1/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let fm = FileManager.default // This is a data type that lets us work with a file system and in case we need to look for more files
+        let path = Bundle.main.resourcePath! // declares a constant called path that is set to the resource path of our apps bundle
+        
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        for item in items {
+            if item.hasPrefix("nssl") {
+                 pictures.append(item)// this is a picture to load !
+            }
+        }
     }
-
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
 }
 
