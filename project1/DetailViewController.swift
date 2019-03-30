abbc35ad32cdcdae8fe6ef5727d36f739402344f
Juan Tenezaca <tenj34@gmail.com>
2019-03-30 12:04:24 -0400

    Add image view controller

---
+++ project1/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  project1
+//
+//  Created by Juan Tenezaca on 3/22/19.
+//  Copyright © 2019 Juan Tenezaca. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
